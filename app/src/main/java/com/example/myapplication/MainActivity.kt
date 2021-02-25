ae74d70866cff0697e83cc55579104cb34ae9cbb
gimidia <gimidia@hotmail.com>
2021-02-25 17:22:45 -0300

    envio do projeto

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+@Composable
+fun NewsStory() {
+
+    MaterialTheme{
+        val typography =MaterialTheme.typography
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ImageHeader()
+            Spacer(Modifier.preferredHeight(16.dp))
+            Text("Outono", style = typography.body2)
+            Text("Inverno", style = typography.body2)
+            Text("Primavera", style = typography.body2)
+            Text("Verão", style = typography.h6)
+            Counter()
+        }
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    NewsStory()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+    Button(onClick = { count.value++ }) {
+        Text("'Cliques' ${count.value} tempos")
+    }
+}
+@Composable
+fun ImageHeader(){
+    val image = imageResource(R.drawable.header)
+    val imageModifier = Modifier
+        .preferredHeight(180.dp)
+        .fillMaxWidth()
+        .clip(shape = RoundedCornerShape(20.dp))
+
+    Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        NewsStory()
+
+    }
+}
