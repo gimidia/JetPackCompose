5e861e561d2f362ff9029f8182df472cce408d44
gimidia <gimidia@hotmail.com>
2021-02-26 09:59:14 -0300

    alteracores

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,13 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -70,6 +68,36 @@ fun ImageHeader(){
         .clip(shape = RoundedCornerShape(20.dp))
 
     Image(image, modifier = imageModifier, contentScale = ContentScale.Crop)
+}
+
+@Composable
+fun ScoreView(score: Score){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        //Parou Aqui
+        Row(
+        modifier = Modifier.padding(bottom = 16.dp)
+        ) {
+
+        }
+    }
+
+
+
+    Button(
+        onClick = {
+            score.homeScore = 0
+            score.visitorScore = 0
+
+        }) {
+        Text("Reset")
+    }
+        
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/myapplication/Score.kt
+++ app/src/main/java/com/example/myapplication/Score.kt
@@ -1,4 +1,17 @@
 package com.example.myapplication
 
-class Score {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+class Score(
+    homeTeam: String,
+    homeScore: Int,
+    visitorTeam: String,
+    visitorScore: Int
+) {
+    var homeTeam by mutableStateOf(homeTeam)
+    var homeScore by mutableStateOf(homeScore)
+    var visitorTeam by mutableStateOf(visitorTeam)
+    var visitorScore by mutableStateOf(visitorScore)
 }
